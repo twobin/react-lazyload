fe9a939e27d8ae78bde721661030a9dee8183024
zemzheng <zemzheng@gmail.com>
2017-06-15 10:04:59 +0800

    fix getParent fail in chrome v59

--- src/utils/scrollParent.js
+++ src/utils/scrollParent.js
@@ -26,7 +26,8 @@ export default (node) => {
       continue;
     }
 
-    if (overflowRegex.test(overflow) && overflowRegex.test(overflowX) && overflowRegex.test(overflowY)) {
+    // https://github.com/jasonslyvia/react-lazyload/issues/84#issuecomment-308607651
+    if ( overflowRegex.test(overflowX) && overflowRegex.test(overflowY)) {
       return parent;
     }
 
