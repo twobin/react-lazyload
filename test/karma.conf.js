0c67bba1aea4997f2670e875afaa85f5830b66aa
jasonslyvia <jasonslyvia@gmail.com>
2017-10-19 17:49:43 +0800

    chore: debug ci again

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -108,7 +108,7 @@ module.exports = function (config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Firefox'],
 
-    browserNoActivityTimeout: 60000,
+    browserNoActivityTimeout: 120000,
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
