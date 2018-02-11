36d044963bc5a93670c9d3ada768cc1731c6ef56
Emmanuel Gautier <git@emmanuelgautier.fr>
2018-02-11 16:50:10 +0100

    Fix CI Tests

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -103,6 +103,12 @@ module.exports = function (config) {
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
